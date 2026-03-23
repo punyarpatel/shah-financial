87e6a5e0f691a34c6fc1975055f5a832fd934209
punyarpatel <punyarpatel4105@gmail.com>
2026-03-23 10:11:33 +0530

    Experiment 8  React ...

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import React from "react";
-import Counter from "./counter";
+import Login from "./Login"; // IMPORTANT
 
 function App() {
   return (
     <div>
-      <h1>React App</h1>
-      <Counter />
+      <Login />
     </div>
   );
 }
